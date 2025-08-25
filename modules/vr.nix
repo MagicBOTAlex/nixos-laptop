8ac0d2a7e1e27e3414db2fad486456611aabf7f8
BOTAlex <zhen@deprived.dev>
2025-08-25 08:36:31 +0200

    Stripped vr.nix from comment bloat

--- modules/vr.nix
+++ modules/vr.nix
@@ -1,25 +1,8 @@
-# Import in home manager
-
 { config, lib, pkgs, ... }: {
-  # programs.envision = {
-  #   enable = true;
-  #   openFirewall = true; # This is set true by default
-  # };
-
   environment.systemPackages = with pkgs; [
     motoc # Quest to PC tracking calibration
     wlx-overlay-s # Requires "--fallback" in sudo nixos-rebuild switch --flake /etc/nixos --impure  --fallback
   ];
-
-  # boot.kernelPatches = [{
-  #   name = "amdgpu-ignore-ctx-privileges";
-  #   patch = pkgs.fetchpatch {
-  #     name = "cap_sys_nice_begone.patch";
-  #     url =
-  #       "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-  #     hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-  #   };
-  # }];
 
   services.wivrn = {
     enable = true;
